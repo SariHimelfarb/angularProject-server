1eff6dcec782547c35a21be14ce819a93b6d0d8d
unknown <s0583223296@gmail.com>
2024-03-14 01:53:39 +0200

    my commit message

---
+++ serverProject/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace serverProject.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? mail { get; set; }
+        public string password { get; set; }
+
+        public string address { get; set; }
+        public User()
+        {
+
+        }
+        public User(int id, string name, string mail, string password, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.mail = mail;
+            this.password = password;
+            this.address = address;
+
+        }
+
+    }
+}
